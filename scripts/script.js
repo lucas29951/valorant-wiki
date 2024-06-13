065a81f2c401a8832ca38ecb7606ffc6018fabe1
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-13 16:07:33 -0300

    Se modifico la estructura de las Cards y se implemento la accion de guardado para mostrar detalles

--- scripts/script.js
+++ scripts/script.js
@@ -20,15 +20,32 @@ function displayItems(items) {
     
     items.forEach(item => {
 
-        let elem = document.createElement('div');
+        let elem = document.createElement('a');
         elem.className = 'card';
+        const arrayColors = item.backgroundGradientColors;
+        elem.style.background = `linear-gradient(#${arrayColors[0]} 0%, #${arrayColors[3]} 100%)`;
+        elem.href = './details.html';
 
         elem.innerHTML = `
-            <img class="img" src="${item.displayIcon}" alt="${item.displayName}">
-                <div class="textBox">
-                  <p class="text head">${item.displayName}</p>
+            <div class="card">
+                <div class="content">
+                    <p class="some">${item.description}</p>
                 </div>
+                <div class="thumb">
+                    <img src="${item.displayIcon}" alt="${item.displayName}">
+                </div>
+                <div class="detial">
+                    <div class="title">
+                        <p class="name">${item.displayName}</p>
+                    </div>
+                </div>
+            </div>
         `;
+
+        elem.addEventListener('click', () => {
+            const data = JSON.parse(elem.getAttribute("data-item"));
+            localStorage.setItem('item-selected', JSON.stringify(item));
+        });
 
         contenedor.appendChild(elem);
     });
