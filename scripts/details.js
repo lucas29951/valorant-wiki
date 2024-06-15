999973e6a648b4661b1c761c59f8759af868ef21
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-14 21:32:39 -0300

    Se modifico el script para mostrar mas detalles

--- scripts/details.js
+++ scripts/details.js
@@ -7,7 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const divInfo = document.createElement('div');
         divInfo.className = 'info';
 
-        const agentName = document.createElement('h2');
+        const subtitle = document.createElement('h3');
+        subtitle.textContent = 'Abilities';
+
+        const agentName = document.createElement('h1');
         agentName.textContent = selectedAgent.displayName;
 
         const agentDescription = document.createElement('p');
@@ -17,10 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
         agentImage.src = selectedAgent.fullPortrait;
         agentImage.alt = selectedAgent.displayName;
 
-        // agentDetailsContainer.appendChild(agentName);
+        const abilities = document.createElement('ul');
+
+        selectedAgent.abilities.forEach(ability => {
+            let elem = document.createElement('li');
+            elem.textContent = `${ability.displayName}`;
+            abilities.appendChild(elem);
+        });
+
         divInfo.appendChild(agentName);
-        // agentDetailsContainer.appendChild(agentDescription);
         divInfo.appendChild(agentDescription);
+        divInfo.appendChild(subtitle);
+        divInfo.appendChild(abilities);
 
         agentDetailsContainer.appendChild(agentImage);
         agentDetailsContainer.appendChild(divInfo);
