2b74035fcbb6561126243e9996d26f647bf22e03
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-18 16:55:30 -0300

    Se agrego el script para la obtencion de datos de las armas

---
+++ scripts/weapons.js
@@ -0,0 +1,53 @@
+
+const URL = 'https://valorant-api.com/v1/';
+const main = document.querySelector('.main');
+const contenedor = document.querySelector('.container');
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const response = await fetch(URL + 'weapons');
+    const res = await response.json();
+
+    const title = document.createElement('h2');
+    title.textContent = 'Weapons';
+    main.appendChild(title);
+
+    displayItems(res.data);
+});
+
+
+function displayItems(items) {
+
+    items.forEach(item => {
+
+        if (item.displayIcon) {
+
+            let elem = document.createElement('a');
+            elem.className = 'card';
+            elem.href = './details.html';
+
+            elem.innerHTML = `
+            <div class="card">
+                <div class="content">
+                    <p class="some">${item.displayName}</p>
+                </div>
+                <div class="thumb">
+                    <img src="${item.displayIcon}" alt="${item.displayName}">
+                </div>
+                <div class="detial">
+                    <div class="title">
+                        <p class="name">${item.displayName}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+            elem.addEventListener('click', () => {
+                const data = JSON.parse(elem.getAttribute("data-item"));
+                localStorage.setItem('item-selected', JSON.stringify(item));
+            });
+
+            contenedor.appendChild(elem);
+        }
+    });
+}
